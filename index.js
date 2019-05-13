c6fb8c3809a3176f8a63389d980513d192e3e6a6
Tyler Ridings <tythedeveloper10@gmail.com>
2019-05-13 11:02:23 -0500

    add time and filter section to vertical flex box

--- index.js
+++ index.js
@@ -37,6 +37,36 @@ Vue.component('dropdown', {
    }
 })
 
+//component for the dropdown
+Vue.component('section-filter', {
+  template:
+  //dropdown parent element, button and content as children
+  //used tick marks so I could write on multiple lines
+  //v-for is used so I can loop through content array in data
+  /*
+  <label class="container">One
+    <input type="checkbox" checked="checked">
+    <span class="checkmark"></span>
+  </label>
+  */
+
+  `<div class="filter-container">
+    <h2>{{ header }}</h2>
+   </div>`,
+
+   //set my text in data function so it can be reactive
+   data: function() {
+     return {
+       header: 'Choose your sections',
+       content: [
+         { text: 'Yesterday' },
+         { text: 'Last 7 Days' },
+         { text: 'Last 30 Days' }
+       ]
+     }
+   }
+})
+
 //create a new Vue instance
 var app = new Vue({
   //point to the main div
