de75fabbf96c010188e081851b5f32f8d0b790b3
Tyler Ridings <tythedeveloper10@gmail.com>
2019-05-12 22:49:12 -0500

    modify component to hold function with data object

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 //component for the navbar
-Vue.component('navbar', {
+/*Vue.component('navbar', {
   template: '<div id="navbar"><h1>Welcome to the NYT Most Viewed!</h1></div>'
+})
+*/
+//component for the navbar
+Vue.component('navbar', {
+  //build a template which we will refer to in html
+  //header text refers to data below
+  template: '<div id="navbar"><h1> {{ header }} </h1></div>',
+  //data has to be a function in component
+  //header text is in component so it can be reactive
+  data: function() {
+    return {
+      header: 'Hello from Vue data!'
+    }
+  }
 })
 
 //create a new Vue instance
