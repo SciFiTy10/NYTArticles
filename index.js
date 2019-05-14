88583c54bdaa4ade80a20a188fcc08d3e7186367
Tyler Ridings <tythedeveloper10@gmail.com>
2019-05-14 18:21:59 -0500

    reorganize vue instance to begin with created and dataLoad method

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ Vue.component('dropdown', {
   `<div class="dropdown">
     <button class="dropbtn">{{ dropdownLabel }}</button>
     <div class="dropdown-content">
-      <a href="#" v-for="c in content">{{ c.text }}</a>
+      <a href="#" v-for="c in content" v-model="dropdownLabel">{{ c.text }}</a>
     </div>
    </div>`,
    //set my text in data function so it can be reactive
@@ -70,19 +70,24 @@ Vue.component('section-filter', {
      }
    }
 })
-
+//props: ['info'],
 //component for the graph section
 Vue.component('graph', {
-  props: ['info'],
+
   template:
   //filter parent container, filter-main area, and filter-checkbox container as children
   //used tick marks so I could write on multiple lines
   //v-for is used so I can loop through content array in data
   `<div class="graph-container">
-    <h3 id="graph-header">{{ info }}</h3>
+    <h3 id="graph-header">{{ header }}</h3>
     <h4 id="graph-subHeader">{{ subHeader }}</h4>
     <canvas id = "viewGraph"></canvas>
    </div>`,
+   methods: {
+     handleClick: function(text){
+       console.log('this is a message from the graph component')
+     }
+   },
 
    //start of mounted
    mounted: function(){
@@ -150,6 +155,7 @@ Vue.component('graph', {
      return {
        header: 'Most Popular NYT Articles of the ',
        subHeader: 'Health / Opinion / U.S.'
+
      }//end of return
    }//end of data
 
@@ -194,39 +200,23 @@ var app = new Vue({
       errored: false
     }
   },
-  mounted () {
-
-    axios
-      .get('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/U.S./1.json?api-key=snmdoMKyQOEYiyJdWwg8F6xodSq8uU7y')
-      .then(response => {
-        this.info = response.data.results[0].section
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
-
-      /*
+  created: function(){
+    this.loadData();
+  },
+  methods: {
+    loadData: function(){
       axios
-        //.get('https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=snmdoMKyQOEYiyJdWwg8F6xodSq8uU7y')
         .get('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/U.S./1.json?api-key=snmdoMKyQOEYiyJdWwg8F6xodSq8uU7y')
-        //.then(response => (this.info = response))
         .then(response => {
-          console.log(response.data.results[0])
-          this.info = response.data.results[0]
+          //this.info = response.data.results[0].section
+          this.info = response.data.results
+          console.log(this.info)
         })
-        */
-  }
-
-  /*
-  mounted () {
-    axios
-      //.get('https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=snmdoMKyQOEYiyJdWwg8F6xodSq8uU7y')
-      .get('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/U.S./1.json?api-key=snmdoMKyQOEYiyJdWwg8F6xodSq8uU7y')
-      //.then(response => (this.info = response))
-      .then(response => console.log(response))
-
-  }
-  */
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+    }//end of loadData
+  }//end of methods
 })
