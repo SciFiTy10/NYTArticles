fd001f07406b942637435bfb8e2a4a0fc7d1803c
Tyler Ridings <tythedeveloper10@gmail.com>
2019-05-13 20:49:38 -0500

    start top-news component

--- index.js
+++ index.js
@@ -86,44 +86,44 @@ Vue.component('graph', {
    //start of mounted
    mounted: function(){
      var ctx = document.getElementById('viewGraph').getContext('2d');
-var myChart = new Chart(ctx, {
-type: 'bar',
-data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-    }]
-},
-options: {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    }
-}
-});
+     var myChart = new Chart(ctx, {
+       type: 'bar',
+       data: {
+         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+         datasets: [{
+           label: '# of Votes',
+           data: [12, 19, 3, 5, 2, 3],
+           backgroundColor: [
+             'rgba(255, 99, 132, 0.2)',
+             'rgba(54, 162, 235, 0.2)',
+             'rgba(255, 206, 86, 0.2)',
+             'rgba(75, 192, 192, 0.2)',
+             'rgba(153, 102, 255, 0.2)',
+             'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+             'rgba(255, 99, 132, 1)',
+             'rgba(54, 162, 235, 1)',
+             'rgba(255, 206, 86, 1)',
+             'rgba(75, 192, 192, 1)',
+             'rgba(153, 102, 255, 1)',
+             'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+         }]
+       },
+       options: {
+         scales: {
+           yAxes: [{
+             ticks: {
+                  beginAtZero: true
+                }
+              }]//end of yAxes
+            }//end of scales
+          }//end of options
+        });//end of chart instance
 
-},//end of mounted function
+      },//end of mounted function
 
    //set my text in data function so it can be reactive
    data: function() {
@@ -133,6 +133,28 @@ options: {
      }//end of return
    }//end of data
 })//end of component
+
+//component for the top-news
+Vue.component('top-news', {
+  template:
+  //dropdown parent element, button and content as children
+  //used tick marks so I could write on multiple lines
+  //v-for is used so I can loop through content array in data
+  `<div class="top-news-container">
+      
+   </div>`,
+   //set my text in data function so it can be reactive
+   data: function() {
+     return {
+       dropdownLabel: 'Select a time period',
+       content: [
+         { text: 'Yesterday' },
+         { text: 'Last 7 Days' },
+         { text: 'Last 30 Days' }
+       ]
+     }
+   }
+})
 
 //create a new Vue instance
 var app = new Vue({
